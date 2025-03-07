bdb9f412a18ba37fc2ae758b32a04cb435e6620e
Kuro <kuroservamp@hotmail.com>
2025-03-07 21:40:22 +0100

    README updates

---
+++ ProgrammList/ConfigManager/PrmListConfigManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace ProgrammList.ConfigManager {
+    public static class PrmListConfigManager {
+
+        public static string GetSetting(string key) {
+            try {
+                ExeConfigurationFileMap configMap = new ExeConfigurationFileMap();
+                Console.WriteLine("Loading " + Directory.GetCurrentDirectory() + "\\app.conf");
+                configMap.ExeConfigFilename = Directory.GetCurrentDirectory() + "\\app.conf";
+                Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+                return config.AppSettings.Settings[key].Value;
+            }
+            catch (Exception e) {
+                Console.WriteLine("Error on Key " + key + ": " + e.ToString());
+                Console.WriteLine(e.ToString());
+            }
+            return "13";
+        }
+
+        public static void SetSetting(string key, string value) {
+            try {
+                ExeConfigurationFileMap configMap = new ExeConfigurationFileMap();
+                Console.WriteLine("Loading " + Directory.GetCurrentDirectory() + "\\app.conf");
+                configMap.ExeConfigFilename = Directory.GetCurrentDirectory() + "\\app.conf";
+                Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+                config.AppSettings.Settings["key"].Value = value;
+            }
+            catch (Exception e) {
+                Console.WriteLine("Error on Key " + key + ": " + e.ToString());
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
